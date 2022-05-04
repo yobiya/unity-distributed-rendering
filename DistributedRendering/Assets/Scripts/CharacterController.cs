fb6abf382464b85205d292e5018e38f9aeae6962
YoBiya <nanashigure@extra.ocn.ne.jp>
2022-05-04 23:05:28 +0900

    WASDで操作対象が移動する機能を追加

--- DistributedRendering/Assets/Scripts/CharacterController.cs
+++ DistributedRendering/Assets/Scripts/CharacterController.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    float _moveDistanceOfSecond = 1.0f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 moveDistance = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveDistance.z += 1;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveDistance.z -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveDistance.x -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveDistance.x += 1;
+        }
+
+        var position = transform.position;
+        transform.position = position + moveDistance.normalized * _moveDistanceOfSecond * Time.deltaTime;
     }
 }
