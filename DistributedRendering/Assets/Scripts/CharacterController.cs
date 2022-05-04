e69c08c9c624021f8ffe9f0e7c93f141fee852fc
YoBiya <nanashigure@extra.ocn.ne.jp>
2022-05-05 01:41:40 +0900

    カメラの向いている方向をキャラクターの移動の前方とするように変更

--- DistributedRendering/Assets/Scripts/CharacterController.cs
+++ DistributedRendering/Assets/Scripts/CharacterController.cs
@@ -5,28 +5,36 @@ public class CharacterController : MonoBehaviour
     [SerializeField]
     float _moveDistanceOfSecond = 1.0f;
 
+    [SerializeField]
+    Transform _cameraTransform;
+
     void Update()
     {
+        // カメラの前方を移動方向の前方とする
+        var forwordDirection = transform.position - _cameraTransform.transform.position;
+        forwordDirection.y = 0.0f;
+        forwordDirection = forwordDirection.normalized;
+
         Vector3 moveDistance = Vector3.zero;
 
         if (Input.GetKey(KeyCode.W))
         {
-            moveDistance.z += 1;
+            moveDistance += forwordDirection;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            moveDistance.z -= 1;
+            moveDistance -= forwordDirection;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            moveDistance.x -= 1;
+            moveDistance -= Vector3.Cross(Vector3.up, forwordDirection).normalized;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            moveDistance.x += 1;
+            moveDistance += Vector3.Cross(Vector3.up, forwordDirection).normalized;
         }
 
         var position = transform.position;
