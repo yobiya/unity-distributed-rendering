e6a3f6b7f5df33ec4277e4f4cc0be809574228a1
YoBiya <nanashigure@extra.ocn.ne.jp>
2022-05-12 01:16:28 +0900

    名前付きパイプの接続を開始するテストコードを追加 内容は未実装なので、テストは通らない

--- DistributedRendering/Assets/Scripts/ProcPart/RenderingServerConnectingProcPart.cs
+++ DistributedRendering/Assets/Scripts/ProcPart/RenderingServerConnectingProcPart.cs
@@ -1,3 +1,13 @@
 public class RenderingServerConnectingProcPart
 {
+    private readonly IRenderingServerConnectingUIViewController _renderingServerConnectingUIViewController;
+    private readonly INamedPipeClient _namedPipeClient;
+
+    public RenderingServerConnectingProcPart(
+        IRenderingServerConnectingUIViewController renderingServerConnectingUIViewController,
+        INamedPipeClient namedPipeClient)
+    {
+        _renderingServerConnectingUIViewController = renderingServerConnectingUIViewController;
+        _namedPipeClient = namedPipeClient;
+    }
 }
--- DistributedRendering/Assets/Tests/ProcPart/RenderingServerConnectingProcPartTest.cs
+++ DistributedRendering/Assets/Tests/ProcPart/RenderingServerConnectingProcPartTest.cs
@@ -1,10 +1,39 @@
+using System;
 using NUnit.Framework;
 using UnityEngine.TestTools;
 
 public class RenderingServerConnectingProcPartTest
 {
+    class TestRenderingServerConnectingUIViewController : IRenderingServerConnectingUIViewController
+    {
+        public event Action OnRequestConnecting;
+
+        public void RequestConnect() {}
+    }
+
+    class TestNamedPipeClient : INamedPipeClient
+    {
+        public bool isCalledConnect = false;
+
+        public bool IsConnecting { get; }
+
+        public void Connect()
+        {
+            isCalledConnect = true;
+        }
+
+        public void Write(string text) {}
+    }
+
     [Test]
     public void StartConnectTest()
     {
+        var renderingServerConnectingUIViewController = new TestRenderingServerConnectingUIViewController();
+        var namedPipeClient = new TestNamedPipeClient();
+        var procPart = new RenderingServerConnectingProcPart(renderingServerConnectingUIViewController, namedPipeClient);
+
+        renderingServerConnectingUIViewController.RequestConnect();
+
+        Assert.IsTrue(namedPipeClient.isCalledConnect);
     }
 }
