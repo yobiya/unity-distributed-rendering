cb784f47ab1ca0a3c37a72cc2e2653b5559f22a0
YoBiya <nanashigure@extra.ocn.ne.jp>
2022-05-12 00:25:18 +0900

    名前付きパイプへの接続用の空のメソッドを追加

--- DistributedRendering/Assets/Scripts/NamedPipe/NamedPipeClient.cs
+++ DistributedRendering/Assets/Scripts/NamedPipe/NamedPipeClient.cs
@@ -1,3 +1,16 @@
 public class NamedPipeClient
 {
+    public bool IsConnecting { get; private set; } = false;
+
+    public NamedPipeClient(string serverName, string pipeName)
+    {
+    }
+
+    public void Connect()
+    {
+    }
+
+    public void Write(string text)
+    {
+    }
 }
