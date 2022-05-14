d63f634e9c9bfb76c2b9dd90cdbfc9cee2339a89
YoBiya <nanashigure@extra.ocn.ne.jp>
2022-05-15 02:17:10 +0900

    ゲームクライアントモードを選択したときに、ゲームクライアント用のUIが表示される処理とテストコードを追加

--- DistributedRendering/Assets/Scripts/ProcPart/GameModeProcPart.cs
+++ DistributedRendering/Assets/Scripts/ProcPart/GameModeProcPart.cs
@@ -1,3 +1,14 @@
 public class GameModeProcPart
 {
+    public GameModeProcPart(
+        IGameModeUIViewController gameModeUIViewController,
+        IRenderingServerConnectingUIViewController renderingServerConnectingUIViewController,
+        IGameClientConnectingWaitUIViewController gameClientConnectingWaitUIViewController)
+    {
+        gameModeUIViewController.OnSelectedGameClientMode += () =>
+        {
+            renderingServerConnectingUIViewController.IsActive = true;
+            gameClientConnectingWaitUIViewController.IsActive = false;
+        };
+    }
 }
