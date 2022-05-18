1db26b093263c55d323fcca03c6b5726b0b86925
YoBiya <nanashigure@extra.ocn.ne.jp>
2022-05-18 21:56:03 +0900

    空のテストクラスを追加

---
+++ DistributedRendering/Assets/Tests/ProcPart/GameClientWaitConnectionProcPartTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+public class GameClientWaitConnectionProcPartTest
+{
+    [Test]
+    public void Activate()
+    {
+    }
+
+    [Test]
+    public void Deactivate()
+    {
+    }
+}
