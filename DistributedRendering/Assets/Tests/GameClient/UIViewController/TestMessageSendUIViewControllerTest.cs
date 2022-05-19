9ff14169f88c8d954067e21bf6ac4fd9b48ebd23
YoBiya <nanashigure@extra.ocn.ne.jp>
2022-05-20 00:31:53 +0900

    テスト用のメッセージをレンダリングサーバーに送るUIの空のテストクラスを追加

---
+++ DistributedRendering/Assets/Tests/GameClient/UIViewController/TestMessageSendUIViewControllerTest.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+public class TestMessageSendUIViewControllerTest
+{
+    [Test]
+    public void Activate()
+    {
+    }
+
+    [Test]
+    public void Deactivate()
+    {
+    }
+
+    [Test]
+    public void Send()
+    {
+    }
+}
