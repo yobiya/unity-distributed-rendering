fb9a712041794dda870a8efc6e152f85e1dafedd
YoBiya <nanashigure@extra.ocn.ne.jp>
2022-05-21 20:30:45 +0900

    TestMessageSendUIViewController用のUICollectionクラスを追加

--- DistributedRendering/Assets/Scripts/GameClient/UIViewController/TestMessageSendUIViewController.cs
+++ DistributedRendering/Assets/Scripts/GameClient/UIViewController/TestMessageSendUIViewController.cs
@@ -2,6 +2,15 @@ using System;
 
 public class TestMessageSendUIViewController : ITestMessageSendUIViewController
 {
+    public interface IUICollection
+    {
+        bool IsActive { get; set; }
+
+        IButtonUIView SendButton { get; }
+    }
+
+    private readonly IUICollection _uiCollection;
+
     public event Action OnSend;
 
     public void Activate()
