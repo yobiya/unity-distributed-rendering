4244f4c62148cad5ca6e1bbe7955242401e99090
YoBiya <nanashigure@extra.ocn.ne.jp>
2022-05-27 23:35:24 +0900

    ServiceLocatorのテストコードを追加

--- DistributedRendering/Assets/Scripts/Common/Utility/ServiceLocator.cs
+++ DistributedRendering/Assets/Scripts/Common/Utility/ServiceLocator.cs
@@ -1,3 +1,17 @@
+using System;
+using System.Collections.Generic;
+
 public class ServiceLocator
 {
+    private Dictionary<Type, object> _instanceContainer = new Dictionary<Type, object>();
+
+    public void Set<T>(object instance)
+    {
+        _instanceContainer.Add(typeof(T), instance);
+    }
+
+    public T Get<T>()
+    {
+        return (T)_instanceContainer[typeof(T)];
+    }
 }
