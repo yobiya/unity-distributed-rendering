b22402439af9b39fbc256ef6fedc5b84c30957bd
YoBiya <nanashigure@extra.ocn.ne.jp>
2022-05-28 00:50:08 +0900

    テストコードを整理

--- DistributedRendering/Assets/Tests/RenderingServer/ProcPart/GameClientWaitConnectionProcPartTest.cs
+++ DistributedRendering/Assets/Tests/RenderingServer/ProcPart/GameClientWaitConnectionProcPartTest.cs
@@ -1,70 +1,67 @@
+using Common;
 using Moq;
 using NUnit.Framework;
 
 public class GameClientWaitConnectionProcPartTest
 {
-    [Test]
-    public void Activate()
+    private TestCollection<GameClientWaitConnectionProcPart> CreateSUT()
     {
+        var serviceLocator = new MockServiceLocator();
+
+        serviceLocator.RegisterMock<IGameClientWaitConnectionUIViewControler>();
+        serviceLocator.RegisterMock<INamedPipeServer>();
+
         var gameClientWaitConnectionUIViewControlerMock = new Mock<IGameClientWaitConnectionUIViewControler>();
         var namedPipeServerMock = new Mock<INamedPipeServer>();
         var sut
             = new GameClientWaitConnectionProcPart(
-                gameClientWaitConnectionUIViewControlerMock.Object,
-                namedPipeServerMock.Object);
+                serviceLocator.Get<IGameClientWaitConnectionUIViewControler>(),
+                serviceLocator.Get<INamedPipeServer>());
+
+        return new TestCollection<GameClientWaitConnectionProcPart>(sut, serviceLocator);
+    }
+
+    [Test]
+    public void Activate()
+    {
+        var collection = CreateSUT();
 
         // 初期状態は有効になっているので、一度無効にする
-        sut.Deactivate();
-        sut.Activate();
+        collection.sut.Deactivate();
+        collection.sut.Activate();
 
-        gameClientWaitConnectionUIViewControlerMock.Verify(m => m.Activate(), Times.Once);
+        collection.serviceLocator.GetMock<IGameClientWaitConnectionUIViewControler>().Verify(m => m.Activate(), Times.Once);
     }
 
     [Test]
     public void Deactivate()
     {
-        var gameClientWaitConnectionUIViewControlerMock = new Mock<IGameClientWaitConnectionUIViewControler>();
-        var namedPipeServerMock = new Mock<INamedPipeServer>();
-        var sut
-            = new GameClientWaitConnectionProcPart(
-                gameClientWaitConnectionUIViewControlerMock.Object,
-                namedPipeServerMock.Object);
+        var collection = CreateSUT();
 
-        sut.Deactivate();
+        collection.sut.Deactivate();
 
-        gameClientWaitConnectionUIViewControlerMock.Verify(m => m.Deactivate(), Times.Once);
+        collection.serviceLocator.GetMock<IGameClientWaitConnectionUIViewControler>().Verify(m => m.Deactivate(), Times.Once);
     }
 
     [Test]
     public void StartWaitClientConnection()
     {
-        var gameClientWaitConnectionUIViewControlerMock = new Mock<IGameClientWaitConnectionUIViewControler>();
-        var namedPipeServerMock = new Mock<INamedPipeServer>();
-        var sut
-            = new GameClientWaitConnectionProcPart(
-                gameClientWaitConnectionUIViewControlerMock.Object,
-                namedPipeServerMock.Object);
+        var collection = CreateSUT();
 
-        sut.StartWaitConnection();
+        collection.sut.StartWaitConnection();
 
-        namedPipeServerMock.Verify(m => m.WaitConnection(), Times.Once);
-        gameClientWaitConnectionUIViewControlerMock.Verify(m => m.ShowWaitConnection(), Times.Once);
+        collection.serviceLocator.GetMock<INamedPipeServer>().Verify(m => m.WaitConnection(), Times.Once);
+        collection.serviceLocator.GetMock<IGameClientWaitConnectionUIViewControler>().Verify(m => m.ShowWaitConnection(), Times.Once);
     }
 
     [Test]
     public void ConnectedClient()
     {
-        var gameClientWaitConnectionUIViewControlerMock = new Mock<IGameClientWaitConnectionUIViewControler>();
-        var namedPipeServerMock = new Mock<INamedPipeServer>();
-        var sut
-            = new GameClientWaitConnectionProcPart(
-                gameClientWaitConnectionUIViewControlerMock.Object,
-                namedPipeServerMock.Object);
+        var collection = CreateSUT();
 
-        sut.StartWaitConnection();
+        collection.sut.StartWaitConnection();
 
-        namedPipeServerMock.Raise(m => m.OnConnected += null);
-
-        gameClientWaitConnectionUIViewControlerMock.Verify(m => m.ShowConnected(), Times.Once);
+        collection.serviceLocator.GetMock<INamedPipeServer>().Raise(m => m.OnConnected += null);
+        collection.serviceLocator.GetMock<IGameClientWaitConnectionUIViewControler>().Verify(m => m.ShowConnected(), Times.Once);
     }
 }
