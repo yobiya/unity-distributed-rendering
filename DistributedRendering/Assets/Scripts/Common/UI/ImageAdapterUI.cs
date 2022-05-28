c7b612c7e40ae5518cb5e73c75d804ef7e7a8322
YoBiya <nanashigure@extra.ocn.ne.jp>
2022-05-28 17:12:04 +0900

    UnityEngine.UI.Imageクラスを扱うためのアダプタクラスとインターフェースを追加

---
+++ DistributedRendering/Assets/Scripts/Common/UI/ImageAdapterUI.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Common
+{
+
+public class ImageAdapterUI : MonoBehaviour, IImageUI
+{
+    [SerializeField]
+    private Image _image;
+}
+
+}
