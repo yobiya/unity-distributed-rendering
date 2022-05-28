1ad7ecd6723e8ff51a78d64182209216fe31d6fd
YoBiya <nanashigure@extra.ocn.ne.jp>
2022-05-28 17:01:33 +0900

    空のデバッグ描画用のImageを配置

--- DistributedRendering/Assets/Tests/SceneTests/RenderingServer/Scene/OffscreenRenderingTestScene.cs
+++ DistributedRendering/Assets/Tests/SceneTests/RenderingServer/Scene/OffscreenRenderingTestScene.cs
@@ -1,7 +1,15 @@
+using Common;
+using RenderingServer;
 using UnityEngine;
 
 public class OffscreenRenderingTestScene : MonoBehaviour
 {
+    [SerializeField]
+    private OffscreenRenderingViewCollection _offscreenRenderingViewCollection;
+
+    [SerializeField]
+    private DebugRenderingUICollection _debugRenderingUICollection;
+
     void Start()
     {
         
