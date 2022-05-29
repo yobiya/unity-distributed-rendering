f5cce15fc14800739530b58eab80dce8ab479545
YoBiya <nanashigure@extra.ocn.ne.jp>
2022-05-29 17:32:26 +0900

    テスト用のボタンを追加

--- DistributedRendering/Assets/Tests/SceneTests/RenderingServer/UI/DebugCommandMessageUI.cs
+++ DistributedRendering/Assets/Tests/SceneTests/RenderingServer/UI/DebugCommandMessageUI.cs
@@ -1,10 +1,17 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace RenderingServer
 {
 
 public class DebugCommandMessageUI : MonoBehaviour, IDebugCommandMessageUI
 {
+    [SerializeField]
+    private Button _renderButton;
+
+    public event Action OnClickedRender;
+
     public void Activate()
     {
     }
--- DistributedRendering/Assets/Tests/SceneTests/RenderingServer/UI/IDebugCommandMessageUI.cs
+++ DistributedRendering/Assets/Tests/SceneTests/RenderingServer/UI/IDebugCommandMessageUI.cs
@@ -1,8 +1,12 @@
+using System;
+
 namespace RenderingServer
 {
 
 public interface IDebugCommandMessageUI
 {
+    event Action OnClickedRender;
+
     void Activate();
     void Deactivate();
 }
