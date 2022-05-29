40dc4f66aacf3cc506459205cfb31ec987a4008e
YoBiya <nanashigure@extra.ocn.ne.jp>
2022-05-29 17:01:29 +0900

    レンダリング画像を送信するテストシーンを追加

---
+++ DistributedRendering/Assets/Tests/SceneTests/RenderingServer/Scene/ResponseRenderingTestScene.cs
@@ -0,0 +1,31 @@
+using Common;
+using RenderingServer;
+using UnityEngine;
+
+public class ResponseRenderingTestScene : MonoBehaviour
+{
+    [SerializeField]
+    private OffscreenRenderingView _offscreenRenderingView;
+
+    [SerializeField]
+    private DebugRenderingUI _debugRenderingUI;
+
+    void Start()
+    {
+        var serviceLocator = new ServiceLocator();
+
+        serviceLocator.Set<IOffscreenRenderingView>(_offscreenRenderingView);
+        serviceLocator.Set<IDebugRenderingUI>(_debugRenderingUI);
+
+        serviceLocator.Set<IOffscreenRenderingViewController>(new OffscreenRenderingViewController(serviceLocator));
+        serviceLocator.Set<IDebugRenderingUIControler>(new DebugRenderingUIControler(serviceLocator));
+
+        var offscreenRenderingProcPart = new OffscreenRenderingProcPart(serviceLocator);
+        var debugRenderingProcPart = new DebugRenderingProcPart(serviceLocator);
+
+        // 表示するテクスチャが準備できたらデバッグレンダリングを有効にする
+        offscreenRenderingProcPart.OnActivated += debugRenderingProcPart.Activate;
+
+        offscreenRenderingProcPart.Activate();
+    }
+}
