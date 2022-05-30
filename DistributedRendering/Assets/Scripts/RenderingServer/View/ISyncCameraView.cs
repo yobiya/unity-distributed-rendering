c7596a8b61ddf5b6f5485298b47213a1ee0383e8
YoBiya <nanashigure@extra.ocn.ne.jp>
2022-05-31 00:08:05 +0900

    空の同期カメラクラスを追加

---
+++ DistributedRendering/Assets/Scripts/RenderingServer/View/ISyncCameraView.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace RenderingServer
+{
+
+public interface ISyncCameraView
+{
+}
+
+}
---
+++ DistributedRendering/Assets/Scripts/RenderingServer/View/SyncCameraView.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace RenderingServer
+{
+
+public class SyncCameraView : MonoBehaviour, ISyncCameraView
+{
+}
+
+}
+
+
