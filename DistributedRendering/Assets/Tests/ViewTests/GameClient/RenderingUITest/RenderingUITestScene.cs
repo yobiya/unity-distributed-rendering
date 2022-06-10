e0a0793df6999f16a2cd525ab609983bca3ca314
YoBiya <nanashigure@extra.ocn.ne.jp>
2022-06-11 00:23:11 +0900

    レンダリングのテストシーンを追加

---
+++ DistributedRendering/Assets/Tests/ViewTests/GameClient/RenderingUITest/RenderingUITestScene.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using Common;
+using MessagePackFormat;
+
+namespace GameClient
+{
+
+public class RenderingUITestScene : MonoBehaviour
+{
+    [SerializeField]
+    private RenderingUI _renderingUI;
+
+    private RenderingUIController _sut;
+
+    void Awake()
+    {
+        var gameObjects = GameObject.FindGameObjectsWithTag(SystemDefinisions.DisableOnLoadTag);
+        foreach (var go in gameObjects)
+        {
+            go.SetActive(false);
+        }
+    }
+
+    void Start()
+    {
+        _sut = new RenderingUIController(_renderingUI);
+
+        var setupData = new SetupData();
+        setupData.renderingRect = new RectInt(RenderingDefinisions.RenderingTextureWidth / 2, 0, RenderingDefinisions.RenderingTextureWidth / 2, RenderingDefinisions.RenderingTextureHight);
+
+        _sut.Activate(setupData);
+    }
+
+    void Update()
+    {
+        // 自分の担当する範囲の描画を行う
+        _sut.RenderBaseImage();
+    }
+}
+
+}
