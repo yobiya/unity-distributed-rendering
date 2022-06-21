dd04a04adf88689e4f57d5dcc6da99a0d6638f77
YoBiya <nanashigure@extra.ocn.ne.jp>
2022-06-22 00:12:19 +0900

    空のキャラクター用のテストシーンを追加

---
+++ DistributedRendering/Assets/Tests/ViewTests/GameClient/CharacterTest/CharacterTestScene.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using Common;
+
+namespace GameClient
+{
+
+public class CharacterTestScene : MonoBehaviour
+{
+    void Awake()
+    {
+        DisableOnLoad.SetUp();
+    }
+
+    void Start()
+    {
+    }
+}
+
+}
