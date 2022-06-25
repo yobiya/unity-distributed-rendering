a1e60ccbca63f9ea638ce8bcb98cf76e80647e31
YoBiya <nanashigure@extra.ocn.ne.jp>
2022-06-25 16:39:24 +0900

    空のキャラクタ関連のクラスを追加

---
+++ DistributedRendering/Assets/Scripts/GameClient/View/CharacterInputView.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace GameClient
+{
+
+public class CharacterInputView : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
+}
---
+++ DistributedRendering/Assets/Scripts/GameClient/View/CharacterView.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace GameClient
+{
+
+public class CharacterView : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
+}
