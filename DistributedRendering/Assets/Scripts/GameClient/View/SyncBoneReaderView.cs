f0c81b1380a2034fd4c43c30f12953dbf14747f3
YoBiya <nanashigure@extra.ocn.ne.jp>
2022-06-26 17:22:13 +0900

    ボーンの読み取り処理を追加

--- DistributedRendering/Assets/Scripts/GameClient/View/SyncBoneReaderView.cs
+++ DistributedRendering/Assets/Scripts/GameClient/View/SyncBoneReaderView.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace GameClient
@@ -5,6 +6,22 @@ namespace GameClient
 
 public class SyncBoneReaderView : MonoBehaviour
 {
+    public List<Transform> ReadTransforms()
+    {
+        var collection = new List<Transform>();
+        CollectTransforms(collection, transform);
+
+        return collection;
+    }
+
+    public void CollectTransforms(List<Transform> collection, Transform transform)
+    {
+        collection.Add(transform);
+        for (int index = 0; index < transform.childCount; ++index)
+        {
+            CollectTransforms(collection, transform.GetChild(index));
+        }
+    }
 }
 
 }
