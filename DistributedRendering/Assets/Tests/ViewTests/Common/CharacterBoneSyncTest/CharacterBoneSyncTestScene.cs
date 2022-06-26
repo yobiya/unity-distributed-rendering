1ab2ebb50fd70e072b046ba3598061045e245863
YoBiya <nanashigure@extra.ocn.ne.jp>
2022-06-26 21:56:31 +0900

    同期ボタンを追加

--- DistributedRendering/Assets/Tests/ViewTests/Common/CharacterBoneSyncTest/CharacterBoneSyncTestScene.cs
+++ DistributedRendering/Assets/Tests/ViewTests/Common/CharacterBoneSyncTest/CharacterBoneSyncTestScene.cs
@@ -1,6 +1,9 @@
 using GameClient;
 using RenderingServer;
 using UnityEngine;
+using UnityEngine.UI;
+using VContainer;
+using VContainer.Unity;
 
 namespace Common
 {
@@ -13,6 +16,16 @@ public class CharacterBoneSyncTestScene : MonoBehaviour
     [SerializeField]
     private SyncBoneWriterView _syncBoneWriterView;
 
+    [SerializeField]
+    private Button ReadSyncTransformButton;
+
+    [SerializeField]
+    private Button WriteSyncTransformButton;
+
+    private IObjectResolver _objectResolver;
+    private SyncTestUIController _syncTestUIController;
+    private SyncTestViewController _syncTestViewController;
+
     void Awake()
     {
         DisableOnLoad.SetUp();
@@ -20,6 +33,22 @@ public class CharacterBoneSyncTestScene : MonoBehaviour
 
     void Start()
     {
+        var containerBuilder = new ContainerBuilder();
+        {
+            // SerializeField
+            containerBuilder.RegisterComponent<SyncBoneReaderView>(_syncBoneReaderView);
+            containerBuilder.RegisterComponent<SyncBoneWriterView>(_syncBoneWriterView);
+
+            // ViewController
+            containerBuilder.Register<SyncTestViewController>(Lifetime.Scoped);
+        }
+
+        _objectResolver = containerBuilder.Build();
+
+        _syncTestUIController = new SyncTestUIController(ReadSyncTransformButton, WriteSyncTransformButton);
+        _syncTestViewController = _objectResolver.Resolve<SyncTestViewController>();
+
+        _syncTestUIController.Activate();
     }
 }
 
--- DistributedRendering/Assets/Tests/ViewTests/Common/CharacterBoneSyncTest/SyncTestUIController.cs
+++ DistributedRendering/Assets/Tests/ViewTests/Common/CharacterBoneSyncTest/SyncTestUIController.cs
@@ -1,3 +1,61 @@
+using System;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using VContainer;
+
+namespace Common
+{
+
 public class SyncTestUIController
 {
+    public event Action OnReadBonesButtonClicked;
+    public event Action OnWriteBonesButtonClicked;
+
+    private readonly Button _readBonesButton;
+    private readonly Button _writeBonesButton;
+
+    private readonly InversionProc _inversionProc = new InversionProc();
+
+    [Inject]
+    public SyncTestUIController(Button readBonesButton, Button writeBonesButton)
+    {
+        _readBonesButton = readBonesButton;
+        _writeBonesButton = writeBonesButton;
+    }
+
+    public void Activate()
+    {
+        UnityAction onReadBonesButtonClicked = () => { OnReadBonesButtonClicked?.Invoke(); };
+        _inversionProc.Register(
+            () =>
+            {
+                _readBonesButton.gameObject.SetActive(true);
+                _readBonesButton.onClick.AddListener(onReadBonesButtonClicked);
+            },
+            () =>
+            {
+                _readBonesButton.gameObject.SetActive(false);
+                _readBonesButton.onClick.RemoveListener(onReadBonesButtonClicked);
+            });
+
+        UnityAction onWriteBonesButtonClicked = () => { OnWriteBonesButtonClicked?.Invoke(); };
+        _inversionProc.Register(
+            () =>
+            {
+                _writeBonesButton.gameObject.SetActive(true);
+                _writeBonesButton.onClick.AddListener(onWriteBonesButtonClicked);
+            },
+            () =>
+            {
+                _writeBonesButton.gameObject.SetActive(false);
+                _writeBonesButton.onClick.RemoveListener(onWriteBonesButtonClicked);
+            });
+    }
+
+    public void Deactivate()
+    {
+        _inversionProc.Inversion();
+    }
+}
+
 }
